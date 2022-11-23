22c72ba5e1c4fb1fffc752fad1f897865e996cc4
bennettk9477 <115193117+bennettk9477@users.noreply.github.com>
2022-11-22 19:32:03 -0500

    Add files via upload

---
+++ P4LABb_Bennett.py
@@ -0,0 +1,18 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("red")
+mason = turtle.Turtle()
+
+penup()
+
+mason.forward(90)
+mason.left(180)
+mason.forward(60)
+mason.left(30)
+mason.forward(20)
+mason.left(180)
+mason.forward(20)
+mason.left(30)
+mason.forward(20)
+
+wn.mainloop()
---
+++ p4LABa_Bennett.py
@@ -0,0 +1,16 @@
+import turtle
+ wn= turtle.Screen()
+ t = turtle.Turtle()
+
+t.forward(50)
+t.left(90)
+t.forward(30)
+t.left(90)
+t.forward(120)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
+wn.mainloop()
+     
