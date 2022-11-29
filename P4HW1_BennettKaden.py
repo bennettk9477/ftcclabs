80d189d1e9424a9b2c39cde8bf186280a7e1a14f
bennettk9477 <115193117+bennettk9477@users.noreply.github.com>
2022-11-28 20:28:22 -0500

    Add files via upload

---
+++ P4HW1_BennettKaden.py
@@ -0,0 +1,56 @@
+#CTI_110
+#P4HW1_ Score List
+#Kaden Bennett
+#11/28/2022
+
+print('How many scores do you want to enter?')
+    score_one = int(input('Enter Score #1:') print(score_one)
+    score_two = int(input('Enter Score #2:') print(score_two)
+    score_three = int(input('Enter Score #3:') print(score_three)
+    grade = if score_avg >= 90
+         print('A')
+            else: if score_avg <= 89
+                print('B')
+                    else: if score_avg <= 79
+                            print('C')
+                                else: if score_avg <= 69
+                                        print('D')
+                                            else: print('F')
+    if score_one <= 100:
+       print(score_one)
+        else:
+            print('INVALID Score entered!!!! Enter score #1')
+    if score_two <= 100:
+       print(score_one)
+        else:
+            print('INVALID Score entered!!!! Enter score #2 again:')
+    if score_three <= 100:
+       print(score_one)
+        else:
+            print('INVALID Score entered!!!! Enter Score #3 again:')
+    print('Score should be between 0 and 100')
+    score_four = int(input('Enter score #4:') print(score_four)
+    score_five = int(input('Enter #5:') print(score_five)
+
+    print('_________Results________')
+        lowest_score =  0
+            print('Lowest Score:') print(lowest_score)
+            if lowest_score <= 40
+                    print(score_one)
+                     else:
+                         print(score_two)
+                         else:
+                             print(score_three)
+                             else:
+                                 print(score_four)
+                                 else:
+                                     print(score_five)
+                        mylist = [score_one, score_two, score_three, score_four, score_five]
+    print('Modified List:') print(mylist)
+    score_avg = mean(mylist)
+    print('Scores Average:') print(score_avg)
+    print('Grade:') print(grade)
+    print('_______________________')
+                            
+    
+        
