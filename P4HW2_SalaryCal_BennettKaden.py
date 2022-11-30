8b274537edc6ee1698072a619df0a2059f230305
bennettk9477 <115193117+bennettk9477@users.noreply.github.com>
2022-11-29 19:16:00 -0500

    Add files via upload

---
+++ P4HW2_SalaryCal_BennettKaden.py
@@ -0,0 +1,24 @@
+#CTI_110
+#P4HW2
+#Kaden Bennett
+#11/29/2022
+#
+
+print('Enter employees name or "None" to terminate:') nameEm = int(input('Enter Name:\n')
+print('How many hours did Nancy Owens worked?') hoursWo = int(input('Enter hours:\n')
+print('What is Nancy Owens pay rate?') payrate = int(input('Enter payrate:\n')
+
+
+print('Employee name:') print(nameEm)
+
+print('Hours Worked') print('Pay Rate')                   print('OverTime')                print('OverTime Pay')                        print('RegHour Pay')                                               print('Gross Pay')
+print('_____________________________________________________________________________________________________________________________________________________________________________________')
+print(hourswo) print(payrate) overti = int(input('Enter OverTime:\n') ovtpay = int(input('Enter Overtime pay:\n') RegHour = int(input('Enter Regular pay:\n') gorss = int(input('Enter gross pay:\n')
+
+print('Enter employees name or "None" to terminate:') emp = int(input('Enter employee name:\n)
+
+print('Total number of employees entered: 2')
+print('Total amount payed for overtime: $138.75')
+print('Total amount payed for regular hours: $1520.00')
+print('Total amount payed in gross: $1658.75')
+                                        
